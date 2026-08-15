b2fba80e1ccd1eaead2adbe62030c684ce6aa1e4
Mukesh Prajapati <mukeshpr443@gmail.com>
2026-08-15 21:04:40 +0545

    feat: name added

--- src/team-b/teamMember20.jsx
+++ src/team-b/teamMember20.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TeamMember10 = () => {
-  const name = ""; // Leave blank for now
+  const name = "Mukesh"; // Leave blank for now
   
   // Return null if name is blank
   if (!name.trim()) {
