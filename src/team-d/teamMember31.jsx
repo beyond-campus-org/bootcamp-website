df466080acfbd8e87b9219520ea4ba118d2156f1
jigneshsifar-jpg <jigneshsifar@gmail.com>
2026-08-16 11:44:41 +0545

    user name added

--- src/team-d/teamMember31.jsx
+++ src/team-d/teamMember31.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TeamMember1 = () => {
-  const name = ""; // Leave blank for now
+  const name = "Jignesh Sifar"; // Leave blank for now
   
   // Return null if name is blank
   if (!name.trim()) {
